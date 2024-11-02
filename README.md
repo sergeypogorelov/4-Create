2932cb1e045a99d95aba618886c3aaa82523bbd8
Sergey Pogorelov <sergey.pogorelov.93@gmail.com>
2024-11-02 16:43:30 +0100

    Update readme, remove placeholder

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.16.
 
+## Demo
+
+The demo is accessible by the following [link](https://sergeypogorelov.github.io/4-Create/).
+
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/4-Create/`. The application will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'user-manager'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('user-manager');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('user-manager app is running!');
-  });
 });
